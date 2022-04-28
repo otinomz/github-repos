79d47ddb7bb4ed7fb7637d88d2fa6bd785376c2b
otinomz <awesomeotinomz@gmail.com>
2022-04-28 19:57:44 +0100

    feat: setup complete

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,3 @@
 export { default as Navbar } from '../components/Navbar/index'
+export { default as RepoCard } from '../components/RepoCard/index'
+export { default as SearchBar } from '../components/SearchBar/index'
