04b4566fa82340372cfa06767624ad54d7b2f9e6
otinomz <awesomeotinomz@gmail.com>
2022-04-28 03:53:02 +0100

    feat: layout components created

---
+++ src/layout/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./index.css"
+
+const Layout = () => {
+    return (
+        <div className="layout">
+            <section className="left">
+                Left go here
+            </section>
+            <section className="right">
+                RIght go here
+            </section>
+        </div>
+    )
+}
+
+export default Layout
