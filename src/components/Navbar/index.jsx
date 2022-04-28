52b578bc1c10deb6d4bc4ed37ff86fca388871ec
otinomz <awesomeotinomz@gmail.com>
2022-04-28 04:27:10 +0100

    feat: navbar list refactored

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import "./index.css"
 
+const List = [
+    {
+        id: 1,
+        name: "Overview"
+    },
+    {
+        id: 2,
+        name: "Repository"
+    },
+    {
+        id: 3,
+        name: "Projects"
+    },
+    {
+        id: 4,
+        name: "Packages"
+    },
+    {
+        id: 5,
+        name: "Stars"
+    },
+]
+
 const Navbar = () => {
+    
+    
     return (
         <nav className="nav">
             <section>
                 Navbar
 
                 <ul className="right__items">
-                    <li>Overview</li>
-                    <li>Repository</li>
-                    <li>Projects</li>
-                    <li>Packages</li>
-                    <li>Stars</li>
+                    { List.map(({id, name}) => (
+                        <li key={id}>{ name }</li>
+                    ))}
                 </ul>
 
             </section>
