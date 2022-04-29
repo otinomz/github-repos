25c973f4f07433961775b3c30c02773b23d59d69
otinomz <awesomeotinomz@gmail.com>
2022-04-29 14:18:50 +0100

    feat: defining the repository end point

--- src/redux/slices/githubSlices.js
+++ src/redux/slices/githubSlices.js
@@ -4,14 +4,16 @@ import axios from "axios"
 
 // create action
 export const fetchRepositoriesAction = ("repositoryList",
-    async (payload, {
+    async (user, {
             rejectWithValue,
             getState,
             dispatch
         }) => {
     
         try {
-            // make HTTP call here
+            // make HTTP call here 
+            const res = await axios.get(`https://api.github.com/users/${user}/repos?per_page=20`)
+
         } catch (error) {
             
         }
