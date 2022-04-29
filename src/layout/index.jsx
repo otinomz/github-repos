5c694fbd48a35482173bfd4f544e2f70d1095e10
otinomz <awesomeotinomz@gmail.com>
2022-04-29 01:11:50 +0100

    chore: reworked on general layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,8 +6,23 @@ const Layout = () => {
         <div className="layout">
             <section className="left">
                 <div className="profile__circle">
-                    
+                    <img src="" alt="profile pics" />
                 </div>
+
+                <div className="profile__description">
+                    <span>otinomz</span>
+                    
+
+                    <div>
+                        <p>One code at a time cos what i cannot create, I do not understand</p>
+                    </div>
+
+                    <button>Edit Profile</button>
+
+                </div>
+                
+
+
             </section>
             
         
