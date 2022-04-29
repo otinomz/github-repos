c1cbe9a75fdf7872d63efc36b8599996c2dd9c71
otinomz <awesomeotinomz@gmail.com>
2022-04-29 14:48:38 +0100

    feat: slice for the repositories handled

--- src/redux/slices/githubSlices.js
+++ src/redux/slices/githubSlices.js
@@ -48,7 +48,26 @@ export const fetchRepositoriesAction = ("repositoryList",
 
 // slices to manage actions and change the state
 const repositoriesSlice = createSlice({
+    name: "repositories",
+    initialState: {},
+    extraReducers: (builder) => {
+        
+        builder.addCase(fetchRepositoriesAction.pending, (state, action) => {
+            state.loading = true
+        });
 
+        builder.addCase(fetchRepositoriesAction.fulfilled, (state, action) => {
+            state.loading = false
+            state.reposList = action?.payload
+            state.error = undefined
+        });
+
+        builder.addCase(fetchRepositoriesAction.rejected, (state, action) => {
+            state.loading = false
+            state.reposList = undefined
+            state.error = action?.payload
+        });
+    }, 
 })
 
 
