3d62f7178cc3b3ee0148ec136a49ba1b57502def
otinomz <awesomeotinomz@gmail.com>
2022-04-29 02:09:59 +0100

    feat: finished with the left layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,11 @@
 import "./index.css"
 import { RepoCard, SearchBar } from '../components'
+import People from '../assets/people.svg'
+import Location from '../assets/location.svg'
+// import Twitter from '../assets/twitter.svg'
+import Globe from "../assets/globe.svg"
+                
+                
 
 const Layout = () => {
     return (
@@ -11,16 +17,39 @@ const Layout = () => {
 
                 <div className="profile__description">
                     <span>otinomz</span>
-                    
-
+                
                     <div>
                         One code at a time cos what i cannot create, I do not understand
                     </div>
 
                     <button>Edit Profile</button>
-
                 </div>
                 
+                <section className="followers__info">
+                    <div>
+                        <img src={People} alt="people icon" />
+                        <span>12 </span> followers
+                    </div>
+
+                    <div>
+                        <img src="" alt="" />
+                        <span>14 </span> following
+                    </div>
+                </section>
+
+                <section className="location__info">
+                    <div>
+                        <img src={Location} alt="location icons" />
+                        Jos North, Nigeria
+                    </div>
+                </section>
+
+               <section className="link__info">
+                    <div>
+                        <img src={Globe} alt="globe icon" />
+                        @otinomz
+                    </div>
+                </section>
 
 
             </section>
