5edb2fb497cc23b3244d93bb54540d2c1dc9cc01
otinomz <awesomeotinomz@gmail.com>
2022-04-30 09:32:41 +0100

    feat: fetching repositories data

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -19,7 +19,6 @@ const Profile = () => {
     console.log(store ) 
     const { loading, profile, error }  = store
     
-
      
     return (
         <>
--- src/components/RepoCard/index.jsx
+++ src/components/RepoCard/index.jsx
@@ -27,119 +27,48 @@ const RepoCard = () => {
 
     return (
         <>
-            <div className="repo__container">
-                <section className="repo__top">
-                    <div className="left__top">
-                        <div className="text">
-                            <a href="#"> github-repos </a>
-                            <button> private </button>
-                        </div>
-                        <p className="description">Deliver food to your favorite restaurant</p>
-                    </div>
-
-                    <div className="left__bottom">
-                        <button>
-                            <img className="star" src={Star} alt="star icon" />
-                            Star
-                        </button>
-
-                        <button className="down">
-                            <img src={Down} alt=" down icon" />
-                        </button>                            
-                    </div>
-                </section>
-                
-                <section className="repo__top">
-                    <div className="left__topz">
-                        <div className="language">
-                            <div className="color"></div>
-                            <p> HTML</p>
-                        </div>
-                        <p>Updated 1 hours ago</p>
-                    </div>
-
-                    <div className="left__bottom">
-                        <div className="chart"></div>                            
-                    </div>
-                </section>
-            </div>
-
             
-            <div className="repo__container">
-                <section className="repo__top">
-                    <div className="left__top">
-                        <div className="text">
-                            <a href="#"> github-repos </a>
-                            <button> private </button>
-                        </div>
-                        <p className="description">Deliver food to your favorite restaurant</p>
+            {
+                repositoriesList?.name !== "Error" && repositoriesList?.map((repositories) => (
+                    <div className="repo__container" key={1}>
+                        <section className="repo__top">
+                            <div className="left__top">
+                                <div className="text">
+                                    <a href="#"> github-repos </a>
+                                    <button> private </button>
+                                </div>
+                                <p className="description">Deliver food to your favorite restaurant</p>
+                            </div>
+
+                            <div className="left__bottom">
+                                <button>
+                                    <img className="star" src={Star} alt="star icon" />
+                                    Star
+                                </button>
+
+                                <button className="down">
+                                    <img src={Down} alt=" down icon" />
+                                </button>                            
+                            </div>
+                        </section>
+                        
+                         <section className="repo__top">
+                            <div className="left__topz">
+                                <div className="language">
+                                    <div className="color"></div>
+                                    <p> HTML</p>
+                                </div>
+                                <p>Updated 1 hours ago</p>
+                            </div>
+
+                            <div className="left__bottom">
+                                <div className="chart"></div>                            
+                            </div>
+                        </section>
                     </div>
-
-                    <div className="left__bottom">
-                        <button>
-                            <img className="star" src={Star} alt="star icon" />
-                            Star
-                        </button>
-
-                        <button className="down">
-                            <img src={Down} alt=" down icon" />
-                        </button>                            
-                    </div>
-                </section>
-                
-                <section className="repo__top">
-                    <div className="left__topz">
-                        <div className="language">
-                            <div className="color"></div>
-                            <p> HTML</p>
-                        </div>
-                        <p>Updated 1 hours ago</p>
-                    </div>
-
-                    <div className="left__bottom">
-                        <div className="chart"></div>                            
-                    </div>
-                </section>
-            </div>
-
-
-
-            <div className="repo__container">
-                <section className="repo__top">
-                    <div className="left__top">
-                        <div className="text">
-                            <a href="#"> github-repos </a>
-                            <button> private </button>
-                        </div>
-                        <p className="description">Deliver food to your favorite restaurant</p>
-                    </div>
-
-                    <div className="left__bottom">
-                        <button>
-                            <img className="star" src={Star} alt="star icon" />
-                            Star
-                        </button>
-
-                        <button className="down">
-                            <img src={Down} alt=" down icon" />
-                        </button>                            
-                    </div>
-                </section>
-                
-                <section className="repo__top">
-                    <div className="left__topz">
-                        <div className="language">
-                            <div className="color"></div>
-                            <p> HTML</p>
-                        </div>
-                        <p>Updated 1 hours ago</p>
-                    </div>
-
-                    <div className="left__bottom">
-                        <div className="chart"></div>                            
-                    </div>
-                </section>
-            </div>
+                ))
+            }
+            
         </>
     )
 }
